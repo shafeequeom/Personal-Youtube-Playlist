4d73ad9651b2a127e8ad184d006c6891ead63e57
shafeequeom <shafeequeom7@gmail.com>
2018-04-28 12:27:38 +0530

    GET data from Mongodb
    API created for viewing all videos and single video by ID

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const Video = require('../models/video');
 
 const db = "mongodb://shafeeque:youtube123@ds261929.mlab.com:61929/youtube-playlist";
 mongoose.Promise = global.Promise;
@@ -9,7 +10,29 @@ mongoose.connect(db, function(err){
         console.error("Error", +err);
     }
 });
-router.get('/', function(req, res){
-    res.send('api works');
+router.get('/videos', function(req, res){
+    console.log('Get request for all videos');
+    Video.find({})
+    .exec(function(err, videos){
+        if(err){
+            console.log("Error retrieving Videos"+err);
+        }else{
+            res.json(videos);
+        }
+    });
+
+});
+
+router.get('/videos/:id', function(req, res){
+    console.log('Get request for single videos');
+    Video.findById(req.params.id)
+    .exec(function(err, video){
+        if(err){
+            console.log("Error retrieving Video"+err);
+        }else{
+            res.json(video);
+        }
+    });
+
 });
 module.exports = router;

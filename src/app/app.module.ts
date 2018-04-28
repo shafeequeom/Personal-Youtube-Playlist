ca0f57a65c588602cb1d434cdbcebb17d0bf05f7
shafeequeom <shafeequeom7@gmail.com>
2018-04-28 14:37:49 +0530

    Angular Routing
    Video Home page and center page created. Basic styling for Nav bar added using bootstrap and paperkit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { VideoCenterComponent } from './video-center/video-center.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    VideoCenterComponent
   ],
   imports: [
     BrowserModule,

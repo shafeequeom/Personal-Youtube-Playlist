2af9107ced41703aba93312f06fc8bd545c31192
shafeequeom <shafeequeom7@gmail.com>
2018-04-28 12:10:40 +0530

    Mongoose Setup
    Connection with mongoDB database using mongoose

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
+const db = "mongodb://shafeeque:youtube123@ds261929.mlab.com:61929/youtube-playlist";
+mongoose.Promise = global.Promise;
+mongoose.connect(db, function(err){
+    if(err){
+        console.error("Error", +err);
+    }
+});
 router.get('/', function(req, res){
     res.send('api works');
 });

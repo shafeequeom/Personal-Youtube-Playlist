c3733cac661d85c479e1cf1a06b199cb73bfad74
shafeequeom <shafeequeom7@gmail.com>
2018-04-28 15:39:09 +0530

    Displaying video list

--- src/app/video-center/video-center.component.ts
+++ src/app/video-center/video-center.component.ts
@@ -1,3 +1,4 @@
+import { Video } from './../video';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoCenterComponent implements OnInit {
 
+  videos: Video[] = [
+    {"_id":"1","title":"Title 1", "url": "url 1", "discription": "desc 1"},
+    {"_id":"2","title":"Title 2", "url": "url 2", "discription": "desc 2"},
+    {"_id":"3","title":"Title 3", "url": "url 3", "discription": "desc 3"},
+    {"_id":"4","title":"Title 4", "url": "url 4", "discription": "desc 4"}
+  ];
   constructor() { }
 
   ngOnInit() {
--- src/app/video-list/video-list.component.ts
+++ src/app/video-list/video-list.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'video-list',
   templateUrl: './video-list.component.html',
-  styleUrls: ['./video-list.component.scss']
+  styleUrls: ['./video-list.component.scss'],
+  inputs:['videos']
 })
 export class VideoListComponent implements OnInit {
 
